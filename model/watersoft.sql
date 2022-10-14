42775949463af1b1d66d8bbe3afb2352e67b34d6
Antonio Medina <antoniomedinamoreno@micorreo.upp.edu.mx>
2022-10-14 13:34:45 -0500

    BD actualizada

--- model/watersoft.sql
+++ model/watersoft.sql
@@ -1,19 +1,25 @@
+drop database watersoft;
+
 CREATE DATABASE IF NOT EXISTS watersoft;
 USE watersoft;
 
-CREATE TABLE Usuario(
+CREATE TABLE IF NOT EXISTS Usuario(
 id_usuario int primary key auto_increment,
 nombre varchar(50),
-tipo_usuario varchar(50),
-contaseña varchar(50)
+tipo_usuario enum('Administrador','Tecnico'),
+contra varchar(50)
 );
 
-CREATE TABLE Deposito(
+CREATE TABLE IF NOT EXISTS Deposito(
 id_deposito int primary key auto_increment,
 cantidad_agua int
 );
 
-CREATE TABLE Llave_principal(
+CREATE TABLE IF NOT EXISTS Llave_principal(
 id_llave int primary key auto_increment,
 estado_llave enum('Abierto','Cerrado')
 );
+
+INSERT INTO usuario(nombre, tipo_usuario, contra) VALUES("Admin", 1, "Admin123");
+
+SELECT * FROM usuario;
